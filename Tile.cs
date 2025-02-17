aa8968dec4a9c6235fd58ab8ebb7288e721954d9
mikaa2909 <mikakandane29@gmail.com>
2025-02-17 21:20:04 +0000

    Update Tile.cs

--- Tile.cs
+++ Tile.cs
@@ -4,9 +4,22 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MAZEGAME
 {
-public class Tile
-{
-    
-}
+    public class Tile
+    {
+        public enum TileType { None, Wall, Ghost, GhostHouse, Player, Snack };
+        public TileType tileType;
+        public Vector2 position;
+
+        public Tile(Vector2 position, TileType tileType)
+        {
+            this.tileType = tileType;
+            this.position = position;
+        }
+
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+    }
 }
  
